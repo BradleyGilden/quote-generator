6d07aac81233ea4b526228d83fc0bb3d5b8f35c7
Bradley Gilden <bradleygilden@gmail.com>
2023-12-19 19:05:09 +0000

    quote generator

---
+++ quote-app/src/index.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
+import {quotes, palettes} from './quotes';
+import './index.css';
+
+
+const Container = function({ text, character, primary, secondary, quoteIcon, handleReload }) {
+  const body = document.querySelector('body');
+  body.style.backgroundColor = secondary;
+  return (
+    <article className='border-4 px-4 py-2 rounded-[20px] w-8/12 sm:w-[500px] text-center'
+    id='quote-box'
+    style={{ backgroundColor: secondary, borderColor: primary }}>
+
+      <p className="font-['Lumos'] text-xl font-semibold max-[400px]:text-sm"
+      id='text'
+      style={{ color: primary }}
+      >{text}
+      </p>
+
+      <p className="text-right font-['Lumos'] font-medium mt-2 max-[400px]:text-xs mr-3"
+      id='author'
+      style={{ color: primary }}>— {character}
+      </p>
+  
+      <div className="flex flex-wrap justify-center gap-x-3 gap-y-2 mt-4">
+  
+        <button id='new-quote'
+        type="button"
+        onClick={handleReload}
+        className={`text-white px-3 py-1 rounded-lg max-[400px]:w-full`} style={{ backgroundColor: primary }}>
+        Generate Quote
+        </button>
+    
+        <a className='text-white px-3 py-1 rounded-lg bg-blue-700 max-[400px]:w-full'
+        id="tweet-quote"
+        href="twitter.com/intent/tweet?text=">
+        Tweet
+        </a>
+      </div>
+    </article>
+  );
+}
+
+function Index() {
+  const [forceUpdate, setForceUpdate] = useState(false);
+
+  const handleReload = () => {
+    setForceUpdate(!forceUpdate);
+  };
+  const quoteIndex = Math.floor(Math.random() * quotes.length);
+  const paletteIndex = Math.floor(Math.random() * palettes.length);
+  const quote = quotes[quoteIndex];
+  const palette = palettes[paletteIndex];
+  return (
+    <div className='w-screen h-screen flex items-center justify-center flex-col gap-14'>
+      <h1 className="block font-['Wizard_World'] text-3xl text-center" style={{ color: palette.primary }}>Harry Potter Quote Generator</h1>
+      <Container {...quote} {...palette} handleReload={handleReload} />
+    </div>
+  );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Index />);
---
+++ quote-app/src/quotes.js
@@ -0,0 +1,106 @@
+
+export const quotes = [
+  {
+    "text": "You're a wizard, Harry.",
+    "character": "Rubeus Hagrid"
+  },
+  {
+    "text": "It does not do to dwell on dreams and forget to live.",
+    "character": "Albus Dumbledore"
+  },
+  {
+    "text": "After all this time? Always.",
+    "character": "Severus Snape"
+  },
+  {
+    "text": "I solemnly swear that I am up to no good.",
+    "character": "Fred and George Weasley"
+  },
+  {
+    "text": "Happiness can be found even in the darkest of times, if one only remembers to turn on the light.",
+    "character": "Albus Dumbledore"
+  },
+  {
+    "text": "The ones that love us never really leave us.",
+    "character": "Sirius Black"
+  },
+  {
+    "text": "Nitwit! Blubber! Oddment! Tweak!",
+    "character": "Albus Dumbledore"
+  },
+  {
+    "text": "I am what I am, an' I'm not ashamed. 'Never be ashamed,' my ol' dad used ter say, 'there's some who'll hold it against you, but they're not worth botherin' with.'",
+    "character": "Rubeus Hagrid"
+  },
+  {
+    "text": "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.",
+    "character": "Albus Dumbledore"
+  },
+  {
+    "text": "Dobby is free.",
+    "character": "Dobby"
+  },
+  {
+    "text": "It is our choices, Harry, that show what we truly are, far more than our abilities.",
+    "character": "Albus Dumbledore"
+  },
+  {
+    "text": "Dark times lie ahead of us, and there will be a time when we must choose between what is easy and what is right.",
+    "character": "Albus Dumbledore"
+  },
+  {
+    "text": "The world had ended, so why had the battle not ceased, the castle fallen silent in horror, and every combatant laid down their arms? Harry's mind was in freefall, spinning out of control, unable to grasp the impossibility, because Fred Weasley could not be dead, the evidence of all his senses must be lying—",
+    "character": "J.K. Rowling (narration)"
+  },
+  {
+    "text": "We must all face the choice between what is right and what is easy.",
+    "character": "Albus Dumbledore"
+  },
+  {
+    "text": "It was, he thought, the difference between being dragged into the arena to face a battle to the death and walking into the arena with your head held high.",
+    "character": "Narration (referring to Harry)"
+  },
+  {
+    "text": "I am not worried, Harry. I am with you.",
+    "character": "Albus Dumbledore"
+  },
+  {
+    "text": "Happiness is found even in the darkest of times if one only remembers to turn on the light.",
+    "character": "Albus Dumbledore"
+  },
+  {
+    "text": "Words are, in my not-so-humble opinion, our most inexhaustible source of magic. Capable of both inflicting injury, and remedying it.",
+    "character": "Albus Dumbledore"
+  },
+  {
+    "text": "You are protected, in short, by your ability to love!",
+    "character": "Albus Dumbledore"
+  },
+  {
+    "text": "The best of us must sometimes eat our words.",
+    "character": "Albus Dumbledore"
+  }
+]
+
+export const palettes = [
+  {
+    'primary': '#56BB67',
+    'secondary': '#2F2F30',
+    'quoteIcon': 'https://img.icons8.com/ios-glyphs/240/56BB67/quote-left.png',
+  },
+  {
+    'primary': '#EECB2D',
+    'secondary': '#B61818',
+    'quoteIcon': 'https://img.icons8.com/ios-glyphs/240/EECB2D/quote-left.png',
+  },
+  {
+    'primary': '#BDBDBD',
+    'secondary': '#2B4575',
+    'quoteIcon': 'https://img.icons8.com/ios-glyphs/240/BDBDBD/quote-left.png',
+  },
+  {
+    'primary': '#625000',
+    'secondary': '#FFD000',
+    'quoteIcon': 'https://img.icons8.com/ios-glyphs/240/625000/quote-left.png',
+  }
+]
